somehow setting eps0=1 seems to give reasonable results. is there still a bug in the units???

transport.py
@@ -23,7 +23,7 @@ class Transport:
     def __init__(self,integrator='FTCS-odeint'):
 
         #THE MESH
-        self.dt=5e-6 #1.0 #5.e-3
+        self.dt=10e-6 #1.0 #5.e-3
         self.dx=80.e-8
         self.tmax=20e-3 #2024.2369851 #10
         self.xmax=80.e-6 #*1e-10
@@ -39,11 +39,11 @@ class Transport:
         self.charges=np.array([1,-1])*unit_F
         self.T = 300
         self.u = np.array([0.0,0.0])
-        self.D = np.array([1.96e-9,1.2e-9]) #np.array([1.5,10.0])/100**2 #in cm2/s
+        self.D = np.array([1.96e-9,1.2e-9])
         self.nspecies=len(self.D)
         self.beta = 1./(self.T * unit_R)
-        self.external_charge=np.zeros([len(self.xmesh)])
-        #self.external_charge=self.gaussian(sigma=0.3,z=1.0,mu=3.0,cmax=0.1) 
+        #self.external_charge=np.zeros([len(self.xmesh)])
+        self.external_charge=self.gaussian(sigma=30e-6,z=1.0,mu=40.e-6,cmax=0.2)
         self.count=1
         self.ax1=plt.subplot('311')
         self.ax2=plt.subplot('312')
@@ -58,7 +58,7 @@ class Transport:
                 dc_dt_boundary={'all':{'all':0.0}},     #in mol/l/s
                 efield_boundary={'r':0.0})    #in V/Ang
 
-        self.initialize='diffusion' #initialize with with diffusion or nothing
+        self.initialize='bla' #diffusion' #initialize with with diffusion or nothing
 
     def run(self):
 
@@ -247,14 +247,16 @@ class Transport:
                     current_charge=self.charges[k]*c[j]
                     if k==0:
                         current_charge+=self.external_charge[i]
-                    total_int+=current_charge*dx/self.eps
+                    total_int+=current_charge*dx
                     efield[i]+=total_int
-                    defield_dx[i]+=current_charge/self.eps
+                    defield_dx[i]+=current_charge
                     #others:
                     self.total_charge[i]+=current_charge
                     self.total_concentrations[k,i]+=c[j]
-            self.total_charge=(self.charges[0]*c[:nx]+self.charges[1]*c[nx:])/self.eps
-            self.defield_dx=self.total_charge
+            efield/=self.eps
+            defield_dx/=self.eps
+#            self.total_charge=(self.charges[0]*c[:nx]+self.charges[1]*c[nx:])/self.eps
+#            self.defield_dx=self.total_charge
             ##integrate charge with Gaussian quadrature, default order=5
             #for k in range(0,self.nspecies):
             #    func=self.charges[k]*c[k*nx:(k+1)*nx]
@@ -322,8 +324,8 @@ class Transport:
                         -self.u[k]*dc_dx \
                         +self.D[k]*(\
                             dc_dx_2 \
-        #                    +self.charges[k]*self.beta*dc_dx*self.efield[i]\
-        #                    +self.charges[k]*self.beta*c[j]*self.defield_dx[i]\
+                            +self.charges[k]*self.beta*dc_dx*self.efield[i]\
+                            +self.charges[k]*self.beta*c[j]*self.defield_dx[i]\
                         )\
                         )
 #            self.ax1.plot(self.xmesh,dc_dt/10**3,'-')
@@ -356,10 +358,12 @@ class Transport:
             c0 = np.zeros([self.nspecies])
             c1 = np.zeros([self.nspecies])
             s = np.zeros([self.nspecies])
+            ee = np.zeros([self.nspecies])
             for k in range(self.nspecies):
                 c0[k] = c[k*nx] # boundary condition on left side
                 c1[k] = c[(k+1)*nx-1] # boundary condition on right side
                 s[k] = self.D[k]*dt/dx**2  # diffusion number
+                ee[k] = self.charges[k]*self.beta*dt*self.D[k]
             # create coefficient matrix:
             def a_matrix(s):
                 return diags([-0.5*s, 1+s, -0.5*s], [-1, 0, 1], 
@@ -395,17 +399,19 @@ class Transport:
             
             for n in range(1,nt): # time is going from second time step to last
                 #self.ax1.plot(self.xmesh,c[:nx],'-')
-                if n<3 and abs(self.charges[0])>0.0:
-                    self.ax1.plot(self.xmesh,c[:nx],self.xmesh,c[nx:])
-                if n==3 and abs(self.charges[0])>0.0:
-                    plt.show()
-                    sys.exit()
+                #if n<50 and abs(self.charges[0])>0.0:
+                #    self.ax1.plot(self.xmesh,c[:nx],label='c1')
+                #    self.ax1.plot(self.xmesh,c[nx:],label='c2')
+                #    self.ax1.legend()
+                #if n==50 and abs(self.charges[0])>0.0:
+                #    plt.show()
+                #    sys.exit()
                 cn = c
                 #PNP electric field modifications
                 self.efield,self.defield_dx=calculate_efield(nx,cn)
-                if n<3 and abs(self.charges[0])>0.0:
-                    self.ax2.plot(self.xmesh,self.efield[:nx],'-')
-                    self.ax3.plot(self.xmesh,self.defield_dx[:nx],'-')
+                #if n<50 and abs(self.charges[0])>0.0:
+                #    self.ax2.plot(self.xmesh,self.efield[:nx],'-')
+                #    self.ax3.plot(self.xmesh,self.defield_dx[:nx],'-')
                 cn_slice=[cc for ic,cc in enumerate(cn) if ((ic+1)%nx!=0 and (ic)%nx!=0)]
 
                 for i in range((nx-2)*self.nspecies):
@@ -419,16 +425,15 @@ class Transport:
                             continue
                         k=i//(nx-2)     #current species
                         ii=i-k*(nx-2)   #current i index
-                        value=self.charges[k]*self.beta*dt*self.D[k]#*unit_F
                         if i==j:
-                            B1[i,j]+=value*self.defield_dx[ii+1]
+                            B1[i,j]+=ee[k]*self.defield_dx[ii+1]
                         if abs(i-j)==1:
                             if j<i:
-                                B1[i,j]-=value*self.efield[ii+1]/4./dx
-                                A[i,j]+=value*self.efield[ii+1]/4./dx
+                                B1[i,j]-=ee[k]*self.efield[ii+1]/4./dx
+                                A[i,j]+=ee[k]*self.efield[ii+1]/4./dx
                             elif i<j:
-                                B1[i,j]+=value*self.efield[ii+1]/4./dx
-                                A[i,j]-=value*self.efield[ii+1]/4./dx
+                                B1[i,j]+=ee[k]*self.efield[ii+1]/4./dx
+                                A[i,j]-=ee[k]*self.efield[ii+1]/4./dx
                 #print '-'*50
                 #print('\n'.join([''.join(['{:4}'.format(item) for item in row])
                 #    for row in A]))
@@ -440,9 +445,18 @@ class Transport:
                 B = np.dot(cn_slice,B1)
                 for k in range(self.nspecies):
                     B[k*(nx-2)] += (0.5*s[k]+\
-                        self.charges[k]*self.beta*dt*self.D[k]*self.efield[0]/4./dx)*(c0[k]+c0[k])
+                        ee[k]*self.efield[0]/4./dx)*(c0[k]+c0[k])
                     B[(k+1)*(nx-2)-1] += (0.5*s[k]-\
-                        self.charges[k]*self.beta*dt*self.D[k]*self.efield[-1]/4./dx)*(c1[k]+c1[k])
+                        ee[k]*self.efield[-1]/4./dx)*(c1[k]+c1[k])
+                    #B[k*(nx-2)] += 0.5*s[k]*(c0[k]+c0[k])
+                    #B[(k+1)*(nx-2)-1] += 0.5*s[k]*(c1[k]+c1[k])
+                #if abs(self.charges[0])>0.0 and n==1:
+                #    for i in range(len(B)):
+                #        print i, B[i]
+                #    for i in range(len(A[:,0])):
+                #        for j in range(len(A[0,:])):
+                #            if A[i,j]!=0.0:
+                #                print i,j,A[i,j]
                 ctmp = np.linalg.solve(A,B) #this gives vector without initial and final elements
                 c = unpack(ctmp,c0,c1,nx) #add left and right boundary values back
                 if n % int(nt/float(ntout)) == 0 or n==nt-1: # or True:
@@ -450,19 +464,30 @@ class Transport:
                     #so we need to write out a copy of c, not c itself
             return cout,s
 
-        def integrate_FTCS(dt,dx,nt,nx,V0):
+        def integrate_FTCS(dt,dx,nt,nx,V0,ntout):
             # diffusion number (has to be less than 0.5 for the 
             # solution to be stable):
-            s = self.D[0]*dt/dx**2
-            V = np.zeros([nt,nx])
-            V[:,0] = [V0[0]]*(nt)       #boundary left  (dc_dt=0)
-            V[:,-1] = [V0[-1]]*(nt)     #boundary right (dc_dt=0)
-            V[0,:]=V0                   #initial
+            V = np.zeros([nt,self.nspecies*nx])
+            ee=np.zeros([self.nspecies])
+            s=np.zeros([self.nspecies])
+            for k in range(self.nspecies):
+                s[k] = self.D[k]*dt/dx**2
+                ee[k] = self.charges[k]*self.beta*dt*self.D[k]
+                V[:,0] = [V0[k*nx]]*(nt)       #boundary left  (dc_dt=0)
+                V[:,-1] = [V0[(k+1)*nx-1]]*(nt)     #boundary right (dc_dt=0)
+            V[0,:]=V0                   #initia
+            cout=[]
             for n in range(0,nt-1): # time
-                for j in range(1,nx-1): # space
-                    V[n+1,j] = V[n,j] + s*(V[n,j-1] -
-                        2*V[n,j] + V[n,j+1]) 
-            cout=V[-1,:]
+                self.efield,self.defield_dx=calculate_efield(nx,V[n,:])
+                for j in range((nx-2)*self.nspecies): # space
+                    k=j//(nx-2)
+                    ii=j-k*(nx-2)   #current i index
+                    V[n+1,j] = V[n,j] + s[k]*(V[n,j-1] -
+                        2*V[n,j] + V[n,j+1]) #+ ee[k]*\
+ #                       (self.efield[ii]*(V[n,j+1]-V[n,j-1])/(2.*dx)+\
+ #                       self.defield_dx[ii]*V[n,j])
+                if n % int(nt/float(ntout)) == 0 or n==nt-1:
+                    cout.append(V.copy())
             return cout
 
         def integrate_Crank_Nicolson(dx,nx,dt,nt,c,ntout):
@@ -488,9 +513,9 @@ class Transport:
 
         if method=='FTCS-odeint':
             cout=integrate_FTCS_odeint(dx,nx,dt,nt,self.c0,ntout)
-            dataplot=cout[-1]
+            dataplot=cout
         elif method=='FTCS':
-            cout=integrate_FTCS(dt,dx,nt,nx,self.c0[:nx])
+            cout=integrate_FTCS(dt,dx,nt,nx,self.c0,ntout)
             dataplot=cout
         elif method=='Crank-Nicolson':
             if self.initialize=='diffusion':