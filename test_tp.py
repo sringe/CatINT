921089bb43e860a5100f331576f035388c8cdd71
Stefan Ringe <sringe@DN0a22b763.SUNet>
2017-10-03 17:36:38 -0700

    modified input script

--- test_tp.py
+++ test_tp.py
@@ -1,7 +1,11 @@
 from transport import Transport
 
 #t=Transport(integrator='Crank-Nicolson')
-#t=Transport(integrator='FTCS-odeint')
-t=Transport(integrator='FTCS')
+t=Transport(
+    integrator='odeint',
+    integrator_correction=False, #True,
+    dt=1e-12,
+    tmax=1e-9)
+#t=Transport(integrator='FTCS')
 t.run()
 t.plot()
