7d786620b1983162ae21fd4fb92ac41ebee7ed9d
sringe <stefan.ringe.tum@gmail.com>
2018-05-16 22:56:04 -0700

    added catint tools

---
+++ tools/catmap_job.py
@@ -0,0 +1,90 @@
+from catmap.model import ReactionModel
+import sys
+from string import Template
+import os
+import numpy as np
+from glob import glob
+
+if len(sys.argv)>2:
+    if sys.argv[-1]=='--cleanup':
+        files=glob('*.png')+glob('*.pdf')+glob('*.pkl')+glob('*.log')
+        for f in files:
+            os.remove(f)
+
+include_rate_control = False
+
+pH = ['0','4','7','10','14']
+i=0
+j = pH[i]
+print 'pH = '+j    
+mkm_template = Template(open('catmap_'+sys.argv[1]+'_template.mkm').read())
+mkm_text = mkm_template.substitute(pH_new = j)
+mkm_file = 'catmap_'+sys.argv[1]+'.mkm'
+with open(mkm_file,'w') as f:
+    f.write(mkm_text)
+model = ReactionModel(setup_file = mkm_file)
+model.output_variables+=['production_rate', 'free_energy', 'selectivity', 'interacting_energy']
+model.run()
+
+from catmap import analyze
+vm = analyze.VectorMap(model)
+#vm.plot_variable = 'rate'
+#vm.descriptor_labels = ['U vs. SHE (V)']
+#vm.log_scale = False
+#vm.min = -100
+#vm.max = 100
+#fig = vm.plot(save=False)
+#fig.savefig('rate'+j+'.png')
+vm.descriptor_labels = ['U vs. SHE (V)']
+vm.plot_variable = 'rate'
+vm.log_scale = True
+vm.min = 1e-10
+vm.max = 1e6
+fig = vm.plot(save=False)
+fig.savefig('rate.pdf')
+
+#vm.plot_variable = 'production_rate'
+#vm.descriptor_labels = ['U vs. SHE (V)']
+#vm.log_scale = True
+#vm.min = -10 #1e-10
+#vm.max = 6 #1e6
+#fig = vm.plot(save=False)
+#fig.savefig('production_rate'+j+'.png')
+vm.descriptor_labels = ['U vs. SHE (V)']
+vm.plot_variable = 'production_rate'
+vm.log_scale = True
+vm.min = 1e-10
+vm.max = 1e6
+fig = vm.plot(save=False)
+fig.savefig('production_rate.pdf')
+
+vm = analyze.VectorMap(model)
+vm.log_scale = False
+vm.plot_variable = 'coverage'
+vm.descriptor_labels = ['coverage (ML)']
+vm.min = 0
+vm.max = 1
+fig = vm.plot(save=False)
+fig.savefig('coverage'+j+'.png')
+
+ma = analyze.MechanismAnalysis(model)
+ma.energy_type = 'interacting_energy'
+label_size = 10
+ma.kwarg_dict = {
+        'VTA':{'label_positions':None,'label_args':{'color':'black','rotation':45,'ha':'center','size':label_size}},
+        'VHA':{'label_positions':None,'label_args':{'color':'blue','rotation':45,'ha':'center','size':label_size}},
+        'VTB':{'label_positions':None,'label_args':{'color':'darkviolet','rotation':45,'ha':'center','size':label_size}},
+        'VHB':{'label_positions':None,'label_args':{'color':'firebrick','rotation':45,'ha':'center','size':label_size}},
+        }
+
+ma.subplots_adjust_kwargs = {'top': 0.87, 'bottom':0.22}
+ma.include_labels = True
+ma.label_args['size'] = 12
+ma.pressure_correction = False
+ma.coverage_correction = False
+fig = ma.plot(save=False, plot_variants = [0.0])
+ax = fig.add_subplot(111)
+ax.set_ylim([-0.5,2.5])
+fig.savefig('FED'+j+'.png')
+
+
---
+++ tools/plotting_catmap.py
@@ -0,0 +1,283 @@
+import os
+import matplotlib.pyplot as plt
+from itertools import cycle
+import sys
+sys.path.insert(0,'/scratch/users/sringe/transport/CatINT')
+import numpy as np
+from glob import glob
+from catint.experimental import EXPDATA
+from matplotlib import rc
+from catint.io import reglob
+import argparse
+from catmap import ReactionModel
+
+parser = argparse.ArgumentParser(description='Process some arguments.')
+
+parser.add_argument('--file',help='file names to be evaluated',nargs='+')
+parser.add_argument('--elemrates',help='If true show the full rate decomposition',action="store_true")
+parser.add_argument('--products',help='Plot only data of selected product',nargs='+')
+parser.add_argument('--pH',help='System pH. If not given, this will be tried to be read from the catmap input file, otherwise set to 7')
+parser.add_argument('--expdata',help='plot also experimental data',action="store_true")
+
+args = parser.parse_args()
+
+print args.products
+
+#first read all variables from one of the mkm files
+
+#rc('text', usetex=False)
+
+exp=EXPDATA()
+
+j_log_plot=True
+
+fig=plt.figure(figsize=(6, 7))
+
+colorlist={}
+colorlist['CO']='r'
+colorlist['H2']='b'
+colorlist['CO2']='k'
+colorlist['CH4']='olive'
+colorlist['CH3CH2OH']='orange'
+colorlist['H']='lightblue'
+colorlist['CHOH']='y'
+colorlist['OCCOH']='darkred'
+colorlist['CHO']='0.75'
+colorlist['OCCO']='g'
+colorlist['COOH']='darkblue'
+symbols={
+        'CO':'x',
+        'H2':'o',
+        'COOH':'d',
+        'HCOOH':'3',
+        'CO2':'D',
+        'CH4':'p',
+        'CH3CH2OH':'1',
+        'CHO':'2'
+        }
+
+
+products=['CO','CH4','CH3CH2OH','H2','HCOOH']
+
+def plot_leis_new_data(ax):
+    #data=np.loadtxt('her_pcCu_lei.csv')
+    i=0
+    for f in ['her_pcCu_lei.csv','her_pcCu_lei_NF_norm.csv','her_pcCu_lei_NF.csv']:
+        i+=1
+        data=np.loadtxt(f)
+        ax.semilogy(data[:,0],10**(data[:,1]),str(i+1),color='k')
+    return ax
+
+def read_data(files,header=False,dtype='species'):
+    #dtype=species or elem (elementary reaction)
+    pdata={}
+    iarg=-1
+    labels=[]
+    for arg2 in files:
+        iarg+=1
+        header_txt=None
+        #product selection
+        if dtype=='species' and args.products is not None:
+            ads=arg2.split('/')[-1].split('_')[1].split('.')[0]
+            if ads not in args.products:
+                continue
+        if dtype=='elem' and args.products is not None:
+            #elementary reaction index of current datafile
+            inx=int(arg2.split('/')[-1].split('_')[2].split('.')[0])
+            #get all product names in which this step plays a role
+            names=[]
+            print 'checking inx = ',inx
+            for sp in model.rxn_mechanisms:
+                indices=model.rxn_mechanisms[sp]
+                if inx in indices:
+                    names.append(sp.split('_')[0])
+            #check if the desired product is any of these 
+            if not any([n in args.products for n in names]):
+                continue
+            else:
+                tmp=model.rxn_expressions[inx-1].split('<->')
+                tmp_txt=''
+                for ii in range(2):
+                    if ii==0:
+                        ik=0
+                    else:
+                        tmp_txt+=' $>$ '
+                        ik=-1
+                    fs_txt=tmp[ik].split(';')[0].replace('*','*').replace('_','\_').split('+')
+                    fs_txt_new=''
+                    for f in fs_txt:
+                        if f.strip() not in ['ele\_g','H\_g','OH\_g','*\_t','*\_s','H2O\_g','*\_dl','*\_g','2*\_t']:
+                            tmp3=f.strip().split('_')
+                            if tmp3[-1] in ['g','s','dl','t']:
+                                tmp2='_'.join(tmp3[:-1]).strip()
+                            else:
+                                tmp2=f
+                            fs_txt_new+=tmp2.rstrip('\\')
+                    tmp_txt+=fs_txt_new
+                header_txt=tmp_txt
+                #if not 'CO2' in header_txt and 'COOH' in header_txt:
+                #    continue
+                
+        if header and header_txt is None:
+            header_txt=arg2.split('_')[-1].split('.')[0]
+#        with open(arg2,'r') as f:
+#            header_txt=[line for iline,line in enumerate(f) if iline==0 and header]
+#            if len(header_txt)>0:
+#                header_txt=header_txt[0]
+#            else:
+#                header_txt=None
+#            if header:
+#                skiprows=1
+#            else:
+#                skiprows=0
+        data=np.loadtxt(arg2) #, skiprows=skiprows)
+        labels.append(header_txt)
+        if len(np.shape(data))>1:
+            x=data[:,0]
+            y=data[:,1]
+        else:
+            x=data[0]
+            y=data[1]
+        species=arg2.split('/')[-1].split('_')[-1].split('.')[0]
+        if species not in pdata:
+            pdata[species]=[]
+        if len(np.shape(data))>1:
+            pdata[species].append([x[0],y[0]])
+        else:
+            pdata[species].append([x,y])
+    for sp in pdata:
+        pdata[sp].sort(key=lambda x: x[0])
+        pdata[sp]=np.array(pdata[sp])
+    return pdata, labels
+
+colors=cycle(['C'+str(i) for i in range(10)])
+linestyles=cycle(['-','--',':','-.'])
+symbols=cycle(['o','x','1','2'])
+k=-1
+for arg in args.file: #sys.argv[1:]:
+    k+=1
+    results_folder=arg+'/catmap_output'
+    linestyle=next(linestyles)
+    
+    ax1=plt.subplot('211')
+    ax2=plt.subplot('212')
+    
+    ax1.set_title('Polarization')
+    searchedfile=glob(arg+'/catmap_input/*/*.mkm')
+    mkm_file = sorted( searchedfile, key = lambda file: os.path.getctime(file))[-1]
+    model = ReactionModel(setup_file = mkm_file)
+    
+    #try to read pH from catmap input file:
+    if args.pH is None:
+        pH=model.pH
+    #    pH = None
+    #    for line in open(catmap_inputs,'r'):
+    #        if line.lstrip().startswith('pH'):
+    #            pH=float(line.split('=')[-1])
+    #            print 'Found pH in catmap input file = ',pH
+    #    if pH is None:
+    #        print 'Found no pH in catmap input file, assuming pH = 7'
+    #        pH = 7
+    else:
+        pH=args.pH
+    pdata,dummy=read_data(glob(results_folder+'/*/j_*'))
+    if args.elemrates:
+        pdata_elem,label_elem=read_data(glob(results_folder+'/*/jelem*'),header=True,dtype='elem')
+    cdata,dummy=read_data(glob(results_folder+'/*/cov*'))
+    for isp,sp in enumerate(pdata):
+        if sp not in products:
+            continue
+        x=pdata[sp][:,0]
+        y=pdata[sp][:,1]
+        color=exp.get_color(sp)
+        if color is None and sp in colorlist:
+            color=colorlist[sp]
+        elif color is None:
+            color='k'
+        symbol=''
+        if isp==len(pdata)-1:
+            symbol='o'
+        if j_log_plot:
+            func=ax1.semilogy
+        else:
+            func=ax1.plot
+        symbol='' #next(symbols)
+        if k==0:
+            func(x+0.059*pH,y,linestyle+symbol,color=color,label=sp) #,label=arg.split('/')[-1])
+        else:
+            func(x+0.059*pH,y,linestyle+symbol,color=color)
+    if args.elemrates:
+        #first read in the rxn mechanism:
+        isp=-1
+        for sp,label in zip(pdata_elem,label_elem):
+            isp+=1
+            x=pdata_elem[sp][:,0]
+            y=pdata_elem[sp][:,1]
+            color=exp.get_color(sp)
+            if color is None and sp in colorlist:
+                color=colorlist[sp]
+            elif color is None:
+                color='k'
+            color=next(colors) #str(0.2+isp*0.2)
+            symbol=''
+            linestyle='--'
+            #if isp==len(pdata)-1:
+            #    symbol='o'
+            #linestyle=next(linestyles)
+            symbol=''
+            if j_log_plot:
+                func=ax1.semilogy
+            else:
+                func=ax1.plot
+            func(x+0.059*pH,y,linestyle+symbol,color=color,label=label) #,label=arg.split('/')[-1])
+    for isp,sp in enumerate(cdata):
+        x=cdata[sp][:,0]
+        y=cdata[sp][:,1]
+        color=exp.get_color(sp)
+        if color is None and sp in colorlist:
+            color=colorlist[sp]
+        elif color is None:
+            color='k'
+        #if sp in symbols:
+        #    symbol=symbols[sp]
+        #else:
+        #    symbol=''
+        #if k==0:
+        #    symbol='x'
+        #else:
+        #    symbol='o'
+        symbol=''
+        if isp==len(cdata)-1:
+            symbol='o'
+        print 'the color',k,sp,color
+        #linestyle=next(linestyles)
+        symbol=next(symbols)
+        if k==0:
+            ax2.plot(x+0.059*pH,y,linestyle+symbol,color=color,label=sp)
+        else:
+            ax2.plot(x+0.059*pH,y,linestyle+symbol,color=color)
+    ax1.legend(fontsize=10)
+#    data=np.loadtxt('data.txt')
+#    ax1.plot(data[:,0],data[:,1],'-')
+    ax2.set_title('Coverages')
+#    iarg=-1
+#    for arg2 in glob(results_folder+'/*/cov*'):
+#        data=np.loadtxt(arg2)
+#        x=data[:,0]
+#        y=data[:,1]
+#        species=arg2.split('/')[-1].split('_')[-1].split('.')[0]
+#        if k==0:
+#            ax2.semilogy(x,y,'o',color=colorlist[species],label=arg2.split('/')[-1])
+#        else:
+#            ax2.semilogy(x,y,'-',color=colorlist[species])
+    ax2.legend(fontsize=10)
+if args.expdata:
+    if pH == 13:
+        exp.plot_data(reference=['hori','jaramillo'],ax=ax1,species=['H$_2$','CO','CH$_4$','C2-sum','HCOOH'],pH=['13.0'],system=['pc-Cu'],scale='RHE',only_points=True,take_log=j_log_plot)
+    elif pH == 6.8 or pH == 7.0:
+        exp.plot_data(reference=['hori','jaramillo'],ax=ax1,species=['H$_2$','CO','CH$_4$','C2-sum','HCOOH'],pH=['6.8','7.0','7.2'],system=['pc-Cu'],scale='RHE',only_points=True,take_log=j_log_plot)
+#ax1=plot_leis_new_data(ax1)
+ax1.set_ylim([1e-8,1e1])
+ax1.set_xlim([-1.6,0.1])
+ax2.set_xlim([-1.6,0.1])
+plt.show()
