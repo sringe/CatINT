7ea0ca14cb1eab278c8627c0c64e321421edc50d
sringe <stefan.ringe.tum@gmail.com>
2019-06-12 14:52:33 +0900

    deleted some old example folders

--- examples/CO2_Carlos/liveplot.gnu
+++
@@ -1,3 +0,0 @@
-plot 'results.txt' u 1:2 w l
-pause 1
-reread
--- examples/CO2_Carlos/test_plot.py
+++
@@ -1,9 +0,0 @@
-from transport.transport import Transport
-from transport.calculator import Calculator
-from transport.plot import Plot
-
-tp=Transport()
-tp.set_calculator('FTCS')
-ca=Calculator(transport=tp)
-pt=Plot(transport=tp)
-pt.plot()
